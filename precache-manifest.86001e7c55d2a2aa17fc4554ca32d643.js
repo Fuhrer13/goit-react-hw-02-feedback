3ff9684969b8df27244debe74c34b5ce6af06efd
Max <54718475+Fuhrer13@users.noreply.github.com>
2020-04-26 15:44:35 +0300

    Updates

---
+++ precache-manifest.86001e7c55d2a2aa17fc4554ca32d643.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f84e8c0cf2ba91967f0a917d4840f1",
+    "url": "/Fuhrer13/goit-react-hw-02-feedback.git/index.html"
+  },
+  {
+    "revision": "f46f747f54f68e317679",
+    "url": "/Fuhrer13/goit-react-hw-02-feedback.git/static/js/2.5ea9fa36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fuhrer13/goit-react-hw-02-feedback.git/static/js/2.5ea9fa36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef4bbdf87a10a6b9a5f",
+    "url": "/Fuhrer13/goit-react-hw-02-feedback.git/static/js/main.cd093ca4.chunk.js"
+  },
+  {
+    "revision": "49912c4c0f158f1003ce",
+    "url": "/Fuhrer13/goit-react-hw-02-feedback.git/static/js/runtime-main.8081ddaa.js"
+  }
+]);
